89993064b9f67a65fde1a38af2b6a4870f2c4a3b
mochizuki-ichiro <mochizukiichiro0@gmail.com>
2017-06-15 17:08:45 +0900

    added index.ejs

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 "use strict";
 // httpを利用するためのモジュール
-const http = require('http');
-const express = require('express');
-const app = express();
+const http = require('http')
+const express = require('express')
+const app = express()
 
 // ルーティングファイルを読み込む
-const index = require('./routes/index');
+const index = require('./routes/index')
 
 // テンプレートファイルを配置したディレクトリを指定
-app.set('views', `${__dirname}/views`);
+app.set('views', `${__dirname}/views`)
 // テンプレートファイルの形式としてejsを指定
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 
 // デフォルトのルーティングとしてindexを指定
-app.use('/', index);
+app.use('/', index)
 // サーバーを定義、Expressを利用する
-const server = http.createServer(app);
+const server = http.createServer(app)
 // ３０００番ポートでリクエストを待ち受ける
-server.listen('3000');erver.listen('3000');
+server.listen('3000')
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 "use strict";
-
-const express = require('express');
+const express = require('express')
 // ルーティングを行うためのモジュール
-const router = express.Router();
-
+const router = express.Router()
 /* GET home page. */
 router.get('/', (req, res, next)=> {
   // テンプレートファイルを使ってHTMLファイルを返す
-  return res.render('index');
-});
+  return res.render('index')
+})
+// require構文を使って外部から読み込みたい時にこれを設定
+module.exports = router
